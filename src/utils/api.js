be6239baf160f9eacc55b346caf45b8c876936b0
MokabbirMisho <mokabbirmisho@gmail.com>
2025-11-25 04:40:30 +0100

    update security

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend URL
-  withCredentials: true, // if you use cookies/JWT in cookies
+  baseURL: import.meta.env.VITE_API_BASE_URL, //  backend URL
+  withCredentials: true, // use cookies/JWT in cookies
 });
 
 export default API;
